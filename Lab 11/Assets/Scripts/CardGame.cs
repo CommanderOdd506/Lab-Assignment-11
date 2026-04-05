012ad910d8fea88724a6cdc68a492a113570cccf
CommanderOdd506 <155345740+CommanderOdd506@users.noreply.github.com>
2026-04-05 13:34:16 -0400

    Lab Finished

--- Lab 11/Assets/Scripts/CardGame.cs
+++ Lab 11/Assets/Scripts/CardGame.cs
@@ -1,7 +1,112 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class CardGame : MonoBehaviour
 {
+    private List<string> deck = new List<string>();
+    private List<string> hand = new List<string>();
+
+    private int handSize = 4;
+
+    private string[] honorCards = { "K", "Q", "J", "A" };
+    private string[] suits = { "\u2660", "\u2663", "\u2665", "\u2666" };
+
+    void Start()
+    {
+        InitializeDeck();
+        InitializeHand();
+        SimulateGame();
+    }
+
+    void InitializeDeck()
+    {
+        for (int i = 0; i < suits.Length; i++)
+        {
+            for (int j = 0; j < honorCards.Length; j++)
+            {
+                string card = honorCards[j] + suits[i];
+                deck.Add(card);
+            }
+        }
+        ShuffleDeck();
+        Debug.Log(string.Join(", ", deck));
+    }
+
+    bool HasWinningHand()
+    {
+        var groups = hand.GroupBy(card => card[card.Length - 1]);
+
+        foreach (var group in groups)
+        {
+            if (group.Count() >= 3)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    void InitializeHand()
+    {
+        for (int i = 0;i < handSize;i++)
+        {
+            hand.Add(deck[0]);
+            deck.RemoveAt(0);
+        }
+
+        Debug.Log("My Hand: " + string.Join(", ", hand));
+    }
+
+    void ShuffleDeck()
+    {
+        deck = deck.OrderBy(x => Random.value).ToList();
+    }
+
+    string CurrentHand()
+    {
+        return string.Join(", ", hand);
+    }
+
+    void SimulateGame()
+    {
+        bool won = false;
+        while (true) 
+        { 
+            if(HasWinningHand())
+            {
+                Debug.Log("My hand is: " + CurrentHand() + ". The game is WON.");
+
+                break;
+            }
+
+            if (deck.Count <= 0)
+            {
+                Debug.Log("The deck is empty. The game is LOST");
+                break;
+            }
+
+            int discardIndex = Random.Range(0, handSize);
+            string discardedCard = hand[discardIndex];
+            hand.RemoveAt(discardIndex);
+
+            string addedCard = deck[0];
+            hand.Add(deck[0]);
+            deck.RemoveAt(0);
+
+            if(HasWinningHand())
+            {
+                Debug.Log($"I discarded {discardedCard} and drew {addedCard}. My hand is: {CurrentHand()}. The game is WON.");
+                break;
+            }
+            else
+            {
+                Debug.Log($"I discarded {discardedCard} and drew {addedCard}. My hand is: {CurrentHand()}. This is not a winning hand. I will attempt to play another round");
+            }
+        }
+
+    }
 }
--- Lab 11/Assets/Scripts/LoginSimulator.cs
+++ Lab 11/Assets/Scripts/LoginSimulator.cs
@@ -1,37 +1,117 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class LoginSimulator : MonoBehaviour
 {
     private static string[] firstNames = { "Todd", "Jesh", "Daniel", "Tyler", "Trevor", "Parker", "Grace", "Jared", "Stephanie", "Alex", "Danny", "Xavier", "Mykaleigh", "Olivia", "Peter", "Wendy", "Pitbull", "Karen", "Mitch", "Max" };
     private static string[] lastNames = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
 
-    private Queue loginQueue;
+    private Queue<string> loginQueue;
     public int startingPlayers = 6;
+
+    public float addInterval;
+    public float subtractInterval;
+
+    //Duplicate Name Detector variables
+
+    string[] namesArray = new string[15];
+    HashSet<string> seen = new HashSet<string>();
+    HashSet<string> duplicates = new HashSet<string>();
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(firstNames.Length);
         InitializeQueue();
-
+        InitializeArray();
     }
 
     void InitializeQueue()
     {
+        loginQueue = new Queue<string> ();
         for (int i = 0; i < startingPlayers; i++)
         {
+            string name = GetRandomName ();
+            loginQueue.Enqueue (name);
+        }
 
+        List<string> queueList = loginQueue.ToList();
+        Debug.Log("Initial login queue created: " + string.Join(", ", queueList));
+
+        InvokeRepeating(nameof(AddNewPlayer), 2.0f, addInterval);
+        InvokeRepeating(nameof(LoginPlayer), 2.0f, subtractInterval);
+    }
+
+
+
+    string GetRandomName()
+    {
+        string firstname = firstNames[Random.Range(0, firstNames.Length)];
+        string lastname = lastNames[Random.Range(0, lastNames.Length)];
+        return firstname + " " + lastname + ".";
+    }
+
+
+
+    void AddNewPlayer()
+    {
+        string name = GetRandomName();
+        loginQueue.Enqueue(name);
+        Debug.Log(name + " is trying to login and added to the login queue.");
+    }
+
+    void LoginPlayer()
+    {
+        if (loginQueue.Count > 0)
+        {
+            string name = loginQueue.Dequeue();
+            Debug.Log(name + " is now inside the game. And queue length is now " +  loginQueue.Count + ".");
+        }
+        else
+        {
+            Debug.Log("Login server is idle. No players are waiting.");
         }
     }
+ 
 
-    void EnqueueRandomPlayer()
+    //
+    // DUPLICATE NAME DETECTOR
+    // 
+
+    void InitializeArray()
     {
+        for (int i = 0; i < 15; i++)
+        {
+            string name = firstNames[Random.Range(0, firstNames.Length)];
+            namesArray[i] = name;
 
+        }
+        Debug.Log(string.Join(", ", namesArray));
+        SortDuplicateNames();
     }
-    // Update is called once per frame
-    void Update()
-    {
 
+    void SortDuplicateNames()
+    {
+        for (int i = 0; i < namesArray.Length; i++)
+        {
+            if (seen.Add(namesArray[i]))
+            {
+
+            }
+            else
+            {
+                duplicates.Add(namesArray[i]);
+            }
+        }
+
+        if(duplicates.Count > 0)
+        {
+            Debug.Log("The array has duplicate names: " + string.Join(", ", duplicates));
+        }
+        else 
+        {
+            Debug.Log("The array has no duplicate names.");
+        }
     }
 }
